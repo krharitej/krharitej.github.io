3eceee81f4f1e25ac612d7ea5545422fd6ba1409
K R HARITEJ <haritejkr@gmail.com>
2024-09-08 12:01:47 +0530

    using BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path = "*" element = {<App/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
